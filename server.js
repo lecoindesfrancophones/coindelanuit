4750ca74451ad4e6f85dfdda3163e7e3a1abd66e
Batfirst <flammyred@yahoo.fr>
2017-09-02 16:55:54 +0200

    test run app

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log('test');
+var colors  = require('colors/safe');
+var express = require('express');
+
+var app 	= express();
+app.listen(8080);
+
+console.log(colors.green('Server running'));
